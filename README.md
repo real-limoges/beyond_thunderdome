0c4af6ca2d2bc0222f14377ea2c1997db3dabbe7
Real Limoges <b.real.limoges@gmail.com>
2015-04-23 21:08:10 -0400

    updating README; adding read file

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a reddit bot that has one purpose: responding to anytime someone uses "Thunderdome" in reddit comments.
+
+Inspired by the classic MST3K sketch : https://www.youtube.com/watch?v=XEI_udV88i4
+
+Because I'm lazy (but in this case, legitimately lazy) I've decided to store the posts I've responded to as a .txt file. Since I'm just having fun it shouldn't be too much of an issue.
---
+++ thunderdome_read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import praw
+
+# naming bot to comply with Reddit API
+user_agent = ("Beyond Thunderdome 0.1")
+
+r = praw.Reddit(user_agent = user_agent)
+
+subreddit = r.get_subreddit("mst3k")
+
+for submission in subreddit.get_hot(limit = 10):
+	print "Title: ", submission.title
+	print "Text: ", submission.selftext
+	print "Score: ", submission.score
+	print " ----------------------- "
